6bb53d9a4321ecd284dba1294c1bf5b29a63d88b
Søren Jessen <skj@nexs.ku.dk>
2025-08-06 13:59:38 +0200

    minor update

--- R/gsea_results.R
+++
@@ -1,5 +0,0 @@
-load(here::here("data/gsea_res_i_filtered.rda"))
-load(here::here("data/gsea_res_ii_filtered.rda"))
-load(here::here("data/gsea_ter_i_filtered.rda"))
-load(here::here("data/gsea_ter_ii_filtered.rda"))
-
