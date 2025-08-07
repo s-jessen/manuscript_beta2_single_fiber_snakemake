cba7d168b56e16aeb47680d4bb14e9dad29e99a3
Søren Jessen <skj@nexs.ku.dk>
2025-08-07 16:29:12 +0200

    Arial font for figures on Windows

--- R/settings.R
+++ R/settings.R
@@ -1,45 +1,40 @@
 library(ggplot2)
 library(svglite)
 
-# Colors
-upregulated_color <- "#a51e22"
-downregulated_color <- "#1e5886"
+#Colors
+upregulated_color = "#a51e22"
+downregulated_color = "#1e5886"
 
 myh7_color <- "#d44a4f"
 myh2_color <- "#7b98cf"
 
-terbutaline_color <- "#C06C84"
-resistance_color <- "#355C7D"
+terbutaline_color = "#C06C84"
+resistance_color = "#355C7D"
 
 res_i_color <- "#e65878"
 res_ii_color <- "#f2a629"
 ter_i_color <- "#113f5b"
 ter_ii_color <- "#785396"
 
-# Set ggplot theme
+#Set ggplot theme
 theme_set(
   theme(
     panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      linewidth = 0.25
-    ),
-    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black",
+                                fill=NA,
+                                linewidth = 0.25),
+    panel.grid.minor=element_blank(),
     panel.grid.major = element_blank(),
     plot.background = element_rect(fill = "white", linewidth = 0),
-    plot.title = element_text(
-      size = 8,
-      margin = margin(0, 0, 0, 0),
-      hjust = 0
-    ),
+    plot.title = element_text(size = 8,
+                              margin = margin(0, 0, 0, 0),
+                              hjust = 0),
     axis.line = element_blank(),
-    axis.text = element_text(color = "black"),
-    axis.title = element_text(size = 8),
-    text = element_text(
-      size = 8,
-      color = "black"
-    ),
+    axis.text = element_text(color = "black", family = "Arial"),
+    axis.title = element_text(size = 8, family = "Arial"),
+    text = element_text(size = 8,
+                        color = "black",
+                        family = "Arial"),
     legend.title = element_blank(),
     legend.key = element_blank(),
     legend.key.size = (unit(3, "mm")),
@@ -49,7 +44,7 @@ theme_set(
     legend.margin = margin(0, 0, -5, 0),
     legend.background = element_blank(),
     legend.box.background = element_blank(),
-    title = element_text(size = 6),
+    title = element_text(size = 6, family = "Arial"),
     strip.background = element_blank()
   )
 )
